c7939635f5acfbde124ca069b639f09e560f2641
jarnvans <jarnvans@student.ateveldehs.be>
2017-10-05 09:14:32 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Oefening variabelen
+        $naam = 'Jarne';
+        echo '<h1>Welkom ' . $naam . '</h1>';
+
+        $string1 = 'hello string';
+    
+    ?>
+</body>
+</html>
